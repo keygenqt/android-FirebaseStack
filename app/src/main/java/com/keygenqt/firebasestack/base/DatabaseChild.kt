28cbb17dfde6eea7ed16930cfeddbea5c1f3fc37
keygenqt <keygenqt@gmail.com>
2021-07-06 22:37:41 +0300

    spotlessApply

--- app/src/main/java/com/keygenqt/firebasestack/base/DatabaseChild.kt
+++ app/src/main/java/com/keygenqt/firebasestack/base/DatabaseChild.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.keygenqt.firebasestack.base
 
 enum class DatabaseChild(name: String) {
--- app/src/main/java/com/keygenqt/firebasestack/data/paging/PageSourceChats.kt
+++ app/src/main/java/com/keygenqt/firebasestack/data/paging/PageSourceChats.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.keygenqt.firebasestack.data.paging
 
 import androidx.paging.PagingSource
